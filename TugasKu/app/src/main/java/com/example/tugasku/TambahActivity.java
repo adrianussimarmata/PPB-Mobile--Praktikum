3bf8274573c16255902836a4900299d1cba65f80
adrianussimarmata <adrianus.simarmata.tif20@polban.ac.id>
2022-04-03 13:37:54 +0700

    Mengambil tanggal dengan DatePicker

--- TugasKu/app/src/main/java/com/example/tugasku/TambahActivity.java
+++ TugasKu/app/src/main/java/com/example/tugasku/TambahActivity.java
@@ -3,12 +3,16 @@ package com.example.tugasku;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class TambahActivity extends AppCompatActivity {
 
@@ -27,6 +31,42 @@ public class TambahActivity extends AppCompatActivity {
         editTextCreate = findViewById(R.id.editTextCreate);
         editTextDeadline = findViewById(R.id.editTextDeadline);
         editTextNote = findViewById(R.id.editTextCatatan);
+
+        // calender class's instance and get current date , month and year from calender
+        Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR); // current year
+        int mMonth = c.get(Calendar.MONTH); // current month
+        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+
+        editTextCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // date picker dialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(TambahActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        // set day of month , month and year value in the edit text
+                        editTextCreate.setText(day + "/" + (month + 1) + "/" + year);
+                    }
+                }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+        editTextDeadline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // date picker dialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(TambahActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        // set day of month , month and year value in the edit text
+                        editTextDeadline.setText(day + "/" + (month + 1) + "/" + year);
+                    }
+                }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
 
         btnNewTask = findViewById(R.id.button_add);
         btnNewTask.setOnClickListener(new View.OnClickListener() {
